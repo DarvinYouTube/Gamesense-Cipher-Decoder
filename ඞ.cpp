6a26ded74ac31f3c0314af1eb42d8262ff6d6f23
DarvinYouTube <85280040+DarvinYouTube@users.noreply.github.com>
2021-06-03 10:48:32 +0300

    Create ඞ.cpp

---
+++ ඞ.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+#define ඞ int
+#define ඞඞ main
+#define ඞඞඞ printf
+#define ඞඞඞඞ "lol"
+#define ඞඞඞඞඞ getchar
+#define ඞඞඞඞඞඞ while
+#define ඞඞඞඞඞඞඞ true
+#define ඞඞඞඞඞඞඞඞ {
+#define ඞඞඞඞඞඞඞඞඞ }
+#define ඞඞඞඞඞඞඞඞඞඞ (
+#define ඞඞඞඞඞඞඞඞඞඞඞ )
+#define ඞඞඞඞඞඞඞඞඞඞඞඞ ;
+
+ඞ ඞඞ ඞඞඞඞඞඞඞඞඞඞ ඞඞඞඞඞඞඞඞඞඞඞ
+ඞඞඞඞඞඞඞඞ
+    ඞඞඞඞඞඞ ඞඞඞඞඞඞඞඞඞඞ ඞඞඞඞඞඞඞ ඞඞඞඞඞඞඞඞඞඞඞ
+    ඞඞඞඞඞඞඞඞ
+        ඞඞඞ ඞඞඞඞඞඞඞඞඞඞ ඞඞඞඞ ඞඞඞඞඞඞඞඞඞඞඞ ඞඞඞඞඞඞඞඞඞඞඞඞ
+    ඞඞඞඞඞඞඞඞඞ
+ඞඞඞඞඞඞඞඞඞ
